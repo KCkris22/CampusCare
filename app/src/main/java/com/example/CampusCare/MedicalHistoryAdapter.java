9f888f5bc0c285a9527060dd46f0bcf9273a4fda
Tung 6x Sahur <magalloncynric@gmail.com>
2025-05-16 13:44:33 +0800

    Update MedicalHistoryAdapter.java

--- app/src/main/java/com/example/CampusCare/MedicalHistoryAdapter.java
+++ app/src/main/java/com/example/CampusCare/MedicalHistoryAdapter.java
@@ -4,44 +4,52 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class MedicalHistoryAdapter extends RecyclerView.Adapter<MedicalHistoryAdapter.ViewHolder> {
 
-    private final List<MedicalHistoryItem> medicalHistoryList;
+    private List<MedicalHistory> medicalHistoryList;
 
-    public MedicalHistoryAdapter(List<MedicalHistoryItem> medicalHistoryList) {
-        this.medicalHistoryList = medicalHistoryList;
+    public MedicalHistoryAdapter(List<MedicalHistory> list) {
+        this.medicalHistoryList = list;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView tvInfo;
+        TextView tvDate, tvDetails;
 
         public ViewHolder(View view) {
             super(view);
-            tvInfo = view.findViewById(R.id.tvInfo);
+            tvDate = view.findViewById(R.id.tvDate);
+            tvDetails = view.findViewById(R.id.tvDetails);
         }
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_medical_history, parent, false);
+    public MedicalHistoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_medical_history, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        MedicalHistoryItem item = medicalHistoryList.get(position);
-        String info = "Name: " + item.name + "\n"
-                + "DOB: " + item.birthDate + "\n"
-                + "Blood Type: " + item.bloodType + "\n"
-                + "Condition: " + item.medicalCondition + "\n"
-                + "Allergies: " + item.allergies + "\n"
-                + "Medications: " + item.medications;
-        holder.tvInfo.setText(info);
+    public void onBindViewHolder(MedicalHistoryAdapter.ViewHolder holder, int position) {
+        MedicalHistory history = medicalHistoryList.get(position);
+
+        holder.tvDate.setText(history.getDateCreated());
+
+        String fullDetails = "Name: " + history.getName() + "\nDOB: " + history.getDob()
+                + "\nBlood Type: " + history.getBloodType()
+                + "\nMedical Conditions: " + history.getMedicalConditions()
+                + "\nAllergies: " + history.getAllergies()
+                + "\nMedications: " + history.getMedications();
+
+        holder.tvDetails.setText(fullDetails);
+        holder.tvDetails.setVisibility(history.isExpanded() ? View.VISIBLE : View.GONE);
+
+        holder.tvDate.setOnClickListener(v -> {
+            history.setExpanded(!history.isExpanded());
+            notifyItemChanged(position);
+        });
     }
 
     @Override
